189a022c7b0ad71b7a0c0bbab940adddfc4c5647
Yavuz Mete Afşar <yavuzmeteafsar@gmail.com>
2024-12-02 21:53:31 +0300

    deneme-2
    2

--- City.java
+++ City.java
@@ -1,2 +1,21 @@
 public class City {
+    private  String cityName;
+
+    public City(){
+        cityName="unkown";
+    }
+    public City(String cityName){
+        this.cityName=cityName;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+    public void diplayCityName(){
+        System.out.println("Displaying city name:"+this.cityName);
+    }
 }
--- CountryMap.java
+++ CountryMap.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CountryMap {
+    private String mapName;
+    private List<City> citys;
+
+    public CountryMap(String mapName){
+        this.mapName= mapName;
+        this.citys= new ArrayList<>();
+    }
+
+    public void addCity(City city){
+        citys.add(city);
+        System.out.println("City added: "+city.getCityName());
+    }
+
 }
