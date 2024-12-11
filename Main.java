9e46c5d57d2de91f9183314557f0b4a66d2ba00d
Yavuz Mete Afşar <yvzmeteafsar@hotmail.com>
2024-12-11 18:12:50 +0300

    update

--- Main.java
+++ Main.java
@@ -2,19 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc=new Scanner(System.in);
+        CountryMap map=new CountryMap("SE115 Map");
+
         System.out.println();
         System.out.println("Welcome to SE115 Maps!");
         System.out.println("Please enter the formatted text:");
         String text= sc.nextLine();
         System.out.println();
+
         String[] pieces=text.split(" ");
         int trial=Integer.parseInt(pieces[0]);
         System.out.println(trial);
         System.out.println(trial+12);
         City city_1=new City(pieces[1]);
-        CountryMap map=new CountryMap("SE115 MAP");
-        map.addCity(city_1);
         city_1.diplayCityName();
 
 
